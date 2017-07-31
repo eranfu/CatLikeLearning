deec05c5eb950669e431287d04a1e5857e0610ad
fuhao <1119369173@qq.com>
2017-08-01 00:55:39 +0800

    RoundedCube

--- Assets/L7RoundedCube/Cube.cs
+++ Assets/L7RoundedCube/Cube.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace L7RoundedCube
 {
@@ -12,30 +13,64 @@ namespace L7RoundedCube
 
         private void Awake()
         {
-            Generate();
+            StartCoroutine(Generate());
         }
 
-        private void Generate()
+        private IEnumerator Generate()
         {
             GetComponent<MeshFilter>().mesh = mesh = new Mesh();
             mesh.name = "Procedural Cube";
+            yield return CreateVertices();
+        }
 
-            var cornerVertices = 8;
+        private IEnumerator CreateVertices()
+        {
+            var wait = new WaitForSeconds(0.05f);
+            const int cornerVertices = 8;
             int edgeVertices = (xSize + ySize + zSize - 3) * 4;
             int faceVertices = 2 * ((xSize - 1) * (ySize - 1) +
                                     (ySize - 1) * (zSize - 1) +
                                     (zSize - 1) * (xSize - 1));
             vertices = new Vector3[cornerVertices + edgeVertices + faceVertices];
             var vi = 0;
-            for (var x = 0; x <= xSize; x++)
+            for (var y = 0; y <= ySize; y++)
             {
-                for (var y = 0; y <= ySize; y++)
+                for (var x = 0; x < xSize; x++)
                 {
-                    for (var z = 0; z <= zSize; z++)
-                    {
-                        if (z != 0 && z != zSize) continue;
-                        vertices[vi++] = new Vector3(x, y, z);
-                    }
+                    vertices[vi++] = new Vector3(x, y, 0);
+                    yield return wait;
+                }
+                for (var z = 0; z < zSize; z++)
+                {
+                    vertices[vi++] = new Vector3(xSize, y, z);
+                    yield return wait;
+                }
+                for (int x = xSize; x > 0; --x)
+                {
+                    vertices[vi++] = new Vector3(x, y, zSize);
+                    yield return wait;
+                }
+                for (int z = zSize; z > 0; --z)
+                {
+                    vertices[vi++] = new Vector3(0, y, z);
+                    yield return wait;
+                }
+            }
+
+            for (var z = 1; z < zSize; z++)
+            {
+                for (var x = 1; x < xSize; x++)
+                {
+                    vertices[vi++] = new Vector3(x, ySize, z);
+                    yield return wait;
+                }
+            }
+            for (var z = 1; z < zSize; z++)
+            {
+                for (var x = 1; x < xSize; x++)
+                {
+                    vertices[vi++] = new Vector3(x, 0, z);
+                    yield return wait;
                 }
             }
         }
