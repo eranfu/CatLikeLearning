f6de3355fee73db15933b44f9715a0d63f9a6d56
fuhao <1119369173@qq.com>
2017-09-03 22:27:58 +0800

    MeshDeformer

---
+++ Assets/L9MeshDeformer/MeshDeformer.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+namespace L9MeshDeformer
+{
+    [RequireComponent(typeof(MeshFilter))]
+    public class MeshDeformer : MonoBehaviour
+    {
+        private Mesh deformingMesh;
+        private Vector3[] originalVertices, displacedVertices;
+        private Vector3[] vertexVelocities;
+
+        private void Start()
+        {
+            deformingMesh = GetComponent<MeshFilter>().mesh;
+            originalVertices = deformingMesh.vertices;
+            displacedVertices = new Vector3[originalVertices.Length];
+            for (var i = 0; i < originalVertices.Length; i++)
+            {
+                displacedVertices[i] = originalVertices[i];
+            }
+            vertexVelocities = new Vector3[originalVertices.Length];
+        }
+
+        private void Update()
+        {
+            for (int i = 0; i < displacedVertices.Length; i++)
+            {
+                updateVertex(i);
+            }
+            deformingMesh.vertices = displacedVertices;
+            deformingMesh.RecalculateNormals();
+        }
+
+        private void updateVertex(int i)
+        {
+            displacedVertices[i] += vertexVelocities[i] * Time.deltaTime;
+        }
+
+        public void addDeformingForce(Vector3 point, float force)
+        {
+            for (int i = 0; i < displacedVertices.Length; i++)
+            {
+                addForceToVertex(i, point, force);
+            }
+        }
+
+        private void addForceToVertex(int i, Vector3 point, float force)
+        {
+            Vector3 pointToVertex = displacedVertices[i] - point;
+            float attenuatedForce = force / (1 + pointToVertex.sqrMagnitude);
+            float velocity = attenuatedForce * Time.deltaTime;
+            vertexVelocities[i] += pointToVertex.normalized * velocity;
+        }
+    }
+}
